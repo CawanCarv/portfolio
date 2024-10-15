266998750bbd5aee864a96ee671fba5e37a1f339
Cawan Carvalho <cawancarvalho3@gmail.com>
2024-10-14 21:50:53 -0300

    Corrige caminho arquivo App.tsx em main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from '/src/components/App.tsx';
-import '/src/styles/index.css';
-import '/src/styles/fonts.css';
+import App from './components/App';
+import './styles/index.css';
+import './styles/fonts.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
