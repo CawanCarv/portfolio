259304362bec8d0dc5f8abc79cb7a251d37cb9cd
Cawan Carvalho <cawancarvalho3@gmail.com>
2024-10-14 21:40:57 -0300

    Adiciona Sessão Projetos

---
+++ src/data/projects.tsx
@@ -0,0 +1,44 @@
+import { Project } from '../interfaces';
+
+export const projects: Array<Project> = [
+  {
+    name: 'Nome do Projeto',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec erat non elit efficitur bibendum sed eget eros. Donec vitae mollis elit, quis lacinia. Integer nec erat non elit efficitur bibendum sed eget eros. Donec vitae mollis elit, quis lacinia.',
+    tags: 'Lorem, ipsum, dolor, sit, amet, consectetur, adipiscing, elit.',
+    link: '#',
+    repositorio: '#',
+  },
+  {
+    name: 'Nome do Projeto',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec erat non elit efficitur bibendum sed eget eros. Donec vitae mollis elit, quis lacinia. Integer nec erat non elit efficitur bibendum sed eget eros. Donec vitae mollis elit, quis lacinia.',
+    tags: 'Lorem, ipsum, dolor, sit, amet, consectetur, adipiscing, elit.',
+    link: '#',
+    repositorio: '#',
+  },
+  {
+    name: 'Nome do Projeto',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec erat non elit efficitur bibendum sed eget eros. Donec vitae mollis elit, quis lacinia. Integer nec erat non elit efficitur bibendum sed eget eros. Donec vitae mollis elit, quis lacinia.',
+    tags: 'Lorem, ipsum, dolor, sit, amet, consectetur, adipiscing, elit.',
+    link: '#',
+    repositorio: '#',
+  },
+  {
+    name: 'Nome do Projeto',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec erat non elit efficitur bibendum sed eget eros. Donec vitae mollis elit, quis lacinia. Integer nec erat non elit efficitur bibendum sed eget eros. Donec vitae mollis elit, quis lacinia.',
+    tags: 'Lorem, ipsum, dolor, sit, amet, consectetur, adipiscing, elit.',
+    link: '#',
+    repositorio: '#',
+  },
+  {
+    name: 'Nome do Projeto',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec erat non elit efficitur bibendum sed eget eros. Donec vitae mollis elit, quis lacinia. Integer nec erat non elit efficitur bibendum sed eget eros. Donec vitae mollis elit, quis lacinia.',
+    tags: 'Lorem, ipsum, dolor, sit, amet, consectetur, adipiscing, elit.',
+    link: '#',
+    repositorio: '#',
+  },
+];
